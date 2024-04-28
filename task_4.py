3ad1ce12e4a9f2297038a6c98250297b7f9aacfd
ggeorge03 <georgeguillain@gmail.com>
2024-04-28 21:43:37 +0100

    AtAdded basic test function.

--- task_4.py
+++ task_4.py
@@ -9,17 +9,17 @@ class Node:
     '''Class for nodes.'''
 
     def __init__(self, value, number, connections=None):
-
+        '''Initialises the nodes'''
         self.index = number
         self.connections = connections
         self.value = value
 
 
 class Network:
-    '''Class for network'''
+    '''Class for network.'''
 
     def __init__(self, nodes=None):
-
+        '''Initialises the network.'''
         if nodes is None:
             self.nodes = []
         else:
@@ -28,9 +28,8 @@ class Network:
     def make_random_network(self, N, connection_probability=0.5):
         '''
         This function makes a *random* network of size N.
-        Each node is connected to each other node with probability p
+        Each node is connected to each other node with probability p.
         '''
-
         self.nodes = []
         for node_number in range(N):
             value = np.random.random()
@@ -87,7 +86,7 @@ class Network:
                     node.connections[i] = new_neighbour
 
     def node_degree(self):
-        '''g'''
+        '''Obtains the degree of each node.'''
         node_degrees = []
         for node in self.nodes:
             degree = sum(node.connections)
@@ -107,11 +106,9 @@ class Network:
         ax.set_ylim([-1.1*network_radius, 1.1*network_radius])
 
         cmap = plt.get_cmap('hot')
-        plt.set_cmap(cmap)
-
         # node_degrees = self.node_degree()
-
-        # cmap = plt.get_cmap('hot')
+        # max_degree = max(node_degrees)
+        plt.set_cmap(cmap)
 
         for (i, node) in enumerate(self.nodes):
             node_angle = i * 2 * np.pi / num_nodes
@@ -122,8 +119,11 @@ class Network:
 
             # node_colour = cmap(normalized_degree)
 
+            # node_degree = node_degrees[i]
+            # circle_colour = cmap(node_degree / max_degree)
+
             # circle = plt.Circle((node_x, node_y), 1.2 * num_nodes,
-            #                     color=node_colour)
+            #                     color=circle_colour)
 
             circle = plt.Circle((node_x, node_y), 1.2 * num_nodes,
                                 color=cmap(node.value))
@@ -140,14 +140,39 @@ class Network:
         plt.show()
 
 
+def test_network():
+    network = Network()
+
+    # Test make_random_network
+    network.make_random_network(10)
+    assert len(
+        network.nodes) == 10, "make_random_network did not create the correct number of nodes"
+
+    # Test make_ring_network
+    network.make_ring_network(10)
+    assert len(
+        network.nodes) == 10, "make_ring_network did not create the correct number of nodes"
+
+    # Test make_small_world_network
+    network.make_small_world_network(10)
+    assert len(
+        network.nodes) == 10, "make_small_world_network did not create the correct number of nodes"
+
+    # Test node_degree
+    # degrees = network.node_degree()
+    # assert len(degrees) == len(
+    #     network.nodes), "node_degree did not return the correct number of degrees"
+
+    print('All tests passed successfully')
+
+
 def main():
     '''g'''
     network = Network()
-    # network.make_random_network(10)
-    # network.plot()
+    test_network()
 
     parser = argparse.ArgumentParser(
-        description='Create plots for a ring network or a small-world network.')
+        description='Create plots for a ring network or a small world network.')
     parser.add_argument('-ring_network', type=int,
                         help='Creates ring network with inputted number of nodes')
     parser.add_argument('-small_world', type=int,
@@ -172,54 +197,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# def test_networks():
-#     '''Tests for networks.'''
-#     # Ring network
-#     nodes = []
-#     num_nodes = 10
-#     for node_number in range(num_nodes):
-#         connections = [0 for val in range(num_nodes)]
-#         connections[(node_number-1) % num_nodes] = 1
-#         connections[(node_number+1) % num_nodes] = 1
-#         new_node = Node(0, node_number, connections=connections)
-#         nodes.append(new_node)
-#     network = Network(nodes)
-
-#     print("Testing ring network")
-#     assert (network.get_mean_degree() == 2), network.get_mean_degree()
-#     assert (network.get_clustering() == 0), network.get_clustering()
-#     assert (network.get_path_length() ==
-#             2.777777777777778), network.get_path_length()
-
-#     nodes = []
-#     num_nodes = 10
-#     for node_number in range(num_nodes):
-#         connections = [0 for val in range(num_nodes)]
-#         connections[(node_number+1) % num_nodes] = 1
-#         new_node = Node(0, node_number, connections=connections)
-#         nodes.append(new_node)
-#     network = Network(nodes)
-
-#     print("Testing one-sided network")
-#     assert (network.get_mean_degree() == 1), network.get_mean_degree()
-#     assert (network.get_clustering() == 0),  network.get_clustering()
-#     assert (network.get_path_length() == 5), network.get_path_length()
-
-#     nodes = []
-#     num_nodes = 10
-#     for node_number in range(num_nodes):
-#         connections = [1 for val in range(num_nodes)]
-#         connections[node_number] = 0
-#         new_node = Node(0, node_number, connections=connections)
-#         nodes.append(new_node)
-#     network = Network(nodes)
-
-#     print("Testing fully connected network")
-#     assert (network.get_mean_degree() ==
-#             num_nodes-1), network.get_mean_degree()
-#     assert (network.get_clustering() == 1),  network.get_clustering()
-#     assert (network.get_path_length() == 1), network.get_path_length()
-
-#     print("All tests passed")

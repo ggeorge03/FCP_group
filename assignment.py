696c40a4c210afdb2f6fcf9d4b292df87250620e
ggeorge03 <georgeguillain@gmail.com>
2024-04-29 11:44:44 +0100

    Added the test_networks function, still needs finishing

--- assignment.py
+++ assignment.py
@@ -166,6 +166,59 @@ class Network:
         plt.show()
 
 
+def test_networks():
+
+    # Ring network
+    nodes = []
+    num_nodes = 10
+    for node_number in range(num_nodes):
+        connections = [0 for val in range(num_nodes)]
+        connections[(node_number-1) % num_nodes] = 1
+        connections[(node_number+1) % num_nodes] = 1
+        new_node = Node(0, node_number, connections=connections)
+        nodes.append(new_node)
+    network = Network(nodes)
+
+    print("Testing ring network")
+    assert (network.get_mean_degree() == 2), network.get_mean_degree()
+    assert (network.get_mean_clustering() == 0), network.get_mean_clustering()
+    assert (network.get_mean_path_length() ==
+            2.777777777777778), network.get_mean_path_length()
+
+    nodes = []
+    num_nodes = 10
+    for node_number in range(num_nodes):
+        connections = [0 for val in range(num_nodes)]
+        connections[(node_number+1) % num_nodes] = 1
+        new_node = Node(0, node_number, connections=connections)
+        nodes.append(new_node)
+    network = Network(nodes)
+
+    print("Testing one-sided network")
+    assert (network.get_mean_degree() == 1), network.get_mean_degree()
+    assert (network.get_mean_clustering() == 0),  network.get_mean_clustering()
+    assert (network.get_mean_path_length() ==
+            5), network.get_mean_path_length()
+
+    nodes = []
+    num_nodes = 10
+    for node_number in range(num_nodes):
+        connections = [1 for val in range(num_nodes)]
+        connections[node_number] = 0
+        new_node = Node(0, node_number, connections=connections)
+        nodes.append(new_node)
+    network = Network(nodes)
+
+    print("Testing fully connected network")
+    assert (network.get_mean_degree() ==
+            num_nodes-1), network.get_mean_degree()
+    assert (network.get_mean_clustering() == 1),  network.get_mean_clustering()
+    assert (network.get_mean_path_length() ==
+            1), network.get_mean_path_length()
+
+    print("All tests passed")
+
+
 # function to calculate neighbour agreement
 def calculate_agreement(population, row, col, external=0.0):
     '''
